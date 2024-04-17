a003295f3843184c80e85ee66c94e67b38c0972a
szy-kenn <johnkennethvelano@gmail.com>
2024-04-17 19:14:08 +0800

    added files

--- fundamentals/FizzBuzz.kt
+++ fundamentals/FizzBuzz.kt
@@ -1,28 +1,71 @@
-fun main() {
+import kotlin.random.Random
+fun fizzbuzz(start: Int, end: Int) {
 
-    var counter = 1
+    var counter = start
+    var fizzbuzzed = false
+    var first = 3
+    var second = 5
 
-    while (counter <= 100) {
+    while (counter <= end) {
 
-        var fizzbuzzed = false
-
-        if (counter % 3 == 0) {
+        if (counter % first == 0) {
             print("Fizz")
             fizzbuzzed = true
         }
-        if (counter % 5 == 0) {
+
+        if (counter % second == 0) {
             print("Buzz")
             fizzbuzzed = true
         }
 
-        counter++
-
-        if (fizzbuzzed) {
-            println()
-            continue
+        if (!fizzbuzzed) {
+            print(counter)
         }
 
-        println(counter-1)
-    }
+        println()
+        counter++;
+        fizzbuzzed = false
 
+    }
+}
+
+fun repeatN(n: Int, action: () -> Unit) {
+    for (i in 0..n-1) {
+        action()
+    }
+}
+
+data class Employee(val empName: String, var salary: Int)
+
+class RandomEmployeeGenerator(var minSalary: Int, var maxSalary: Int) {
+
+    val generateEmployee = { Employee("Ken", Random.nextInt(minSalary, maxSalary))}
+}
+
+fun employeeById(id: Int): Employee? = when(id) {
+    1 -> Employee("Mary", 20)
+    2 -> null
+    3 -> Employee("John", 21)
+    4 -> Employee("Ann", 23)
+    else -> null
+}
+
+fun salaryById(id: Int): Int = (employeeById(id)?.salary) ?: 0
+
+fun main() {
+//    fizzbuzz(50, 100)
+
+    val actions = listOf<String>("title", "year", "author")
+    val prefix = "https://example.com/book-info"
+    val id = 5
+    val urls = actions.map { "$prefix/$id/$it" }
+
+    val emp1 = Employee("Mary", 20)
+    println(emp1)
+    emp1.salary += 10
+    println(emp1)
+
+    val empGen = RandomEmployeeGenerator(10, 30)
+
+     println((1..5).sumOf { salaryById(it) })
 }
--- fundamentals/Test.kt
+++ fundamentals/Test.kt
@@ -1,14 +1,44 @@
+import kotlin.math.PI as pi
+
+fun startsWith(list: List<String>, c: Char) {
+    for (item in list) {
+        if (item.startsWith(c)) {
+            println(item)
+        }
+    }
+}
+
+fun circleArea(r: Int): Double = pi * (r * r)
+
+fun intervalInSeconds(h: Int = 0, m: Int = 0, s: Int = 0): Int {
+    return ((h * 60) + m) * 60 + s;
+}
 fun main() {
 
+    // lists and maps (immutable variable = val)
     val list: List<String> = listOf("test0", "test1", "test2")
     val map: Map<String, Int> = mapOf("apple" to 1, "banana" to 2, "orange" to 3)
 
+    // mutable variables
     var x = 5
     var y = 2
 
+    // range
     val numbers = (1..4)
 
-    for (number: Int in numbers) {
-        print("Number: $number \n")
-    }
+    // for loop
+//    for (number: Int in numbers) {
+//      print("Number: $number \n")
+//    }
+
+    // call function
+    val randomList = listOf<String>("dinosaur", "lemon", "chair", "television")
+
+    val area = circleArea(5)
+
+    val interval = intervalInSeconds(s=10)
+    print(interval)
+
+    println({string: String -> string.uppercase()} ("hello, kotlin!"))
+
 }
